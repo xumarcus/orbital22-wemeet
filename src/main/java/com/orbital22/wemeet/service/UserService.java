ef40f902640836cbfec4ab1bc32ffec8d8847f89
marcus <marcus@workforceoptimizer.com>
2022-06-04 04:11:59 +0800

    Cleanup

--- src/main/java/com/orbital22/wemeet/service/UserService.java
+++ src/main/java/com/orbital22/wemeet/service/UserService.java
@@ -4,12 +4,13 @@ import com.orbital22.wemeet.model.User;
 import com.orbital22.wemeet.repository.UserRepository;
 import com.orbital22.wemeet.security.AclRegisterService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.acls.domain.BasePermission;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import javax.transaction.Transactional;
+
+import static org.springframework.security.acls.domain.BasePermission.*;
 
 @Service
 @Transactional
@@ -17,14 +18,12 @@ import javax.transaction.Transactional;
 public class UserService {
   private PasswordEncoder passwordEncoder;
   private UserRepository userRepository;
-
   private AclRegisterService aclRegisterService;
 
   public void register(String email, String password) {
     Assert.isTrue(!userRepository.existsByEmail(email), "Email exists");
     User user = User.ofRegistered(email, passwordEncoder.encode(password));
     userRepository.save(user);
-    aclRegisterService.register(
-        user, email, BasePermission.READ, BasePermission.WRITE, BasePermission.DELETE);
+    aclRegisterService.register(user, email, READ, WRITE, DELETE);
   }
 }
