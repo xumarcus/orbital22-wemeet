6c89b7d93bc27697133b7b2e0022fd621b8fc009
marcus <marcus@workforceoptimizer.com>
2022-06-07 20:59:42 +0800

    Expose aclService to test

---
+++ src/main/java/com/orbital22/wemeet/config/AclServiceConfig.java
@@ -0,0 +1,64 @@
+package com.orbital22.wemeet.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.acls.domain.*;
+import org.springframework.security.acls.jdbc.BasicLookupStrategy;
+import org.springframework.security.acls.jdbc.JdbcMutableAclService;
+import org.springframework.security.acls.jdbc.LookupStrategy;
+import org.springframework.security.acls.model.AclCache;
+import org.springframework.security.acls.model.PermissionGrantingStrategy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import javax.sql.DataSource;
+
+@AllArgsConstructor
+@Configuration
+public class AclServiceConfig {
+  private final DataSource dataSource;
+  private final CacheManager cacheManager;
+
+  @Bean
+  public AclAuthorizationStrategy aclAuthorizationStrategy() {
+    // sysadmin
+    return new AclAuthorizationStrategyImpl(new SimpleGrantedAuthority("ROLE_ADMIN"));
+  }
+
+  @Bean
+  public PermissionGrantingStrategy permissionGrantingStrategy() {
+    return new DefaultPermissionGrantingStrategy(new ConsoleAuditLogger());
+  }
+
+  @Bean
+  public AclCache aclCache() {
+    return new SpringCacheBasedAclCache(
+        cacheManager.getCache("aclCache"),
+        permissionGrantingStrategy(),
+        aclAuthorizationStrategy());
+  }
+
+  @Bean
+  public LookupStrategy lookupStrategy() {
+    return new BasicLookupStrategy(
+        dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
+  }
+
+  @Profile("test")
+  @Bean
+  public JdbcMutableAclService testAclService() {
+    return new JdbcMutableAclService(dataSource, lookupStrategy(), aclCache());
+  }
+
+  @Profile("!test") // PostgresQL
+  @Bean
+  public JdbcMutableAclService aclService() {
+    JdbcMutableAclService aclService =
+        new JdbcMutableAclService(dataSource, lookupStrategy(), aclCache());
+    aclService.setClassIdentityQuery("select currval(pg_get_serial_sequence('acl_class', 'id'))");
+    aclService.setSidIdentityQuery("select currval(pg_get_serial_sequence('acl_sid', 'id'))");
+    return aclService;
+  }
+}
