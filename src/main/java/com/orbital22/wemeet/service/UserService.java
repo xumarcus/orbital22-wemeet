37128056469baca1ccdc4be97bc9888ee13fa7db
marcus <marcus@workforceoptimizer.com>
2022-06-07 21:04:12 +0800

    Log user in (WEM-15)

--- src/main/java/com/orbital22/wemeet/service/UserService.java
+++ src/main/java/com/orbital22/wemeet/service/UserService.java
@@ -1,21 +1,26 @@
 package com.orbital22.wemeet.service;
 
+import com.orbital22.wemeet.dto.AuthRegisterRequest;
 import com.orbital22.wemeet.model.User;
 import com.orbital22.wemeet.repository.UserRepository;
 import com.orbital22.wemeet.security.AclRegisterService;
+import com.orbital22.wemeet.security.CustomUser;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.security.Principal;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.springframework.security.acls.domain.BasePermission.*;
 
-@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -23,25 +28,44 @@ public class UserService {
   private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
   private final AclRegisterService aclRegisterService;
+  private final AuthenticationManager authenticationManager;
 
-  public User register(String email, String password) {
-    if (userRepository.existsByEmail(email)) {
-      throw new RuntimeException();
+  public Optional<User> register(AuthRegisterRequest request) {
+    if (userRepository.existsByEmail(request.getEmail())) {
+      return Optional.empty(); // Validated before
     }
-    User user = userRepository.save(User.ofRegistered(email, passwordEncoder.encode(password)));
-    aclRegisterService.register(user, email, READ, WRITE, DELETE);
-    log.info("Email " + email + " registered.");
-    return user;
+
+    User user =
+        userRepository.save(
+            User.ofRegistered(request.getEmail(), passwordEncoder.encode(request.getPassword())));
+
+    UsernamePasswordAuthenticationToken authReq =
+        new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+    SecurityContextHolder.getContext()
+        .setAuthentication(authenticationManager.authenticate(authReq));
+
+    aclRegisterService.register(user, user.getEmail(), READ, WRITE, DELETE);
+    return Optional.of(user);
   }
 
-  public Set<User> makeFromEmails(Collection<String> emails) {
+  Set<User> fromEmails(Collection<String> emails) {
     Set<User> users = userRepository.findByEmailIn(emails);
-    Set<String> remainingEmails = users.stream().map(User::getEmail).collect(Collectors.toSet());
     users.addAll(
-        emails.stream()
-            .filter(remainingEmails::contains)
-            .map(User::ofUnregistered)
+        userRepository
+            .saveAll(
+                () ->
+                    emails.stream()
+                        .filter(
+                            email -> users.stream().map(User::getEmail).noneMatch(email::equals))
+                        .map(User::ofUnregistered)
+                        .iterator())
+            .stream()
+            .peek(user -> aclRegisterService.register(user, user.getEmail(), READ, WRITE, DELETE))
             .collect(Collectors.toSet()));
     return users;
+  }
+
+  public Optional<User> fromPrincipal(Principal principal) {
+    return userRepository.findById(((CustomUser) principal).getId());
   }
 }
