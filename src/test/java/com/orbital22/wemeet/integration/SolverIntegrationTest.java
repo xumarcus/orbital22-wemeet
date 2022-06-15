38caf2d6eb4c99a846e2da43f57e496b42ddeae5
marcus <marcus@workforceoptimizer.com>
2022-06-15 22:12:22 +0800

    Make tests run faster

--- src/test/java/com/orbital22/wemeet/integration/SolverIntegrationTest.java
+++ src/test/java/com/orbital22/wemeet/integration/SolverIntegrationTest.java
@@ -94,54 +94,6 @@ public class SolverIntegrationTest {
   @Test
   public void contextLoads() {}
 
-  private void addFirstTimeSlot() throws Exception {
-    Map<String, Object> map = new HashMap<>();
-    map.put("startDateTime", "2019-06-09T18:00:00");
-    map.put("endDateTime", "2019-06-09T19:00:00");
-    map.put("capacity", 2);
-    map.put("rosterPlan", "/api/rosterPlan/1");
-
-    this.mockMvc
-        .perform(
-            post("/api/timeSlot")
-                .with(user(talk.getEmail()))
-                .content(objectMapper.writeValueAsString(map))
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andExpect(redirectedUrl("http://localhost/api/timeSlot/1"));
-  }
-
-  private void addSecondTimeSlot() throws Exception {
-    Map<String, Object> map = new HashMap<>();
-    map.put("startDateTime", "2019-06-12T18:00:00");
-    map.put("endDateTime", "2019-06-12T19:00:00");
-    map.put("capacity", 1);
-    map.put("rosterPlan", "/api/rosterPlan/1");
-
-    this.mockMvc
-        .perform(
-            post("/api/timeSlot")
-                .with(user(talk.getEmail()))
-                .content(objectMapper.writeValueAsString(map))
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andExpect(redirectedUrl("http://localhost/api/timeSlot/2"));
-  }
-
-  private void addTimeSlotUserInfo(User user, int timeSlotId, int rank) throws Exception {
-    Map<String, Object> map = new HashMap<>();
-    map.put("timeSlot", "/api/timeSlot/" + timeSlotId);
-    map.put("rank", rank);
-
-    this.mockMvc
-        .perform(
-            post("/api/timeSlotUserInfo")
-                .with(user(user.getEmail()))
-                .content(objectMapper.writeValueAsString(map))
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated());
-  }
-
   @Test
   public void givenTimeSlotUserInfos_whenAddChild_thenSolverRuns(
       @Autowired TimeSlotRepository timeSlotRepository,
