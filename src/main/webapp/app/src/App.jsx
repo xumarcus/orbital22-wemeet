8adba161f97c7e06023af19a40790e192ca89ce2
marcus <marcus@workforceoptimizer.com>
2022-06-17 03:25:37 +0800

    Force maxWidth on app

--- src/main/webapp/app/src/App.jsx
+++ src/main/webapp/app/src/App.jsx
@@ -3,15 +3,16 @@ import CssBaseline from '@mui/material/CssBaseline';
 import {Outlet} from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Container from '@mui/material/Container';
 
 const App = () => {
   return (
       <>
         <CssBaseline/>
         <NavBar/>
-        <React.Fragment>
+        <Container maxWidth={'xl'}>
           <Outlet/>
-        </React.Fragment>
+        </Container>
         <Footer/>
       </>
   );
