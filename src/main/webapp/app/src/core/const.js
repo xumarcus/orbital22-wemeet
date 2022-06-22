6e9a28569a775cb2b16ebda0064c2b531e8391e4
marcus <marcus@workforceoptimizer.com>
2022-06-23 02:50:15 +0800

    Add error messages / pages

--- src/main/webapp/app/src/core/const.js
+++ src/main/webapp/app/src/core/const.js
@@ -2,6 +2,15 @@ export const SYNCFUSION_LICENSE_KEY = 'ORg4AjUWIQA/Gnt2VVhhQlFaclhJXGFWfVJpTGpQd
 export const TOOLBAR = ['Add', 'Edit', 'Delete', 'Update', 'Cancel']
 export const API = {
   ROSTER_PLAN: '/api/rosterPlan',
+  ROSTER_PLAN_ID: (id) => `/api/rosterPlan/${id}`,
   ROSTER_PLAN_USER_INFO: '/api/rosterPlanUserInfo',
-  TIME_SLOT: '/api/timeSlot'
+  TIME_SLOT: '/api/timeSlot',
+  ME: '/api/users/me',
+}
+export const ROUTES = {
+  MEETING_EDIT: (id) => `/meeting/${id}/edit`,
+  MEETING_RANK: (id) => `/meeting/${id}/rank`,
+}
+export const ERROR_MESSAGES = {
+  INVALID_URL: "Invalid URL",
 }
