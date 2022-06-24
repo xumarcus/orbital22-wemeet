b0d90284c56e912f619c44b308fd6479be93880e
marcus <marcus@workforceoptimizer.com>
2022-06-25 06:55:07 +0800

    Hotfix

--- src/main/webapp/app/src/pages/MeetingEdit.jsx
+++ src/main/webapp/app/src/pages/MeetingEdit.jsx
@@ -1,19 +1,29 @@
 import * as React from 'react'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import PageTitle from '../components/PageTitle'
 import { useParams } from 'react-router-dom'
 import ErrorFallback from '../components/ErrorFallback'
 import { ErrorBoundary } from 'react-error-boundary'
 import CustomScheduleComponent from '../components/CustomScheduleComponent'
 import useSWR from 'swr'
-import { CircularProgress } from '@mui/material'
+import {
+  CircularProgress,
+  Divider,
+  FormHelperText,
+  Select,
+} from '@mui/material'
 import ajax from '../core/ajax'
 import InvitationGrid from '../components/InvitationGrid'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { API, ERROR_MESSAGES } from '../core/const'
-import AppContext, { defaultAppContext } from '../core/AppContext'
-import _ from 'lodash'
+import AppContext from '../core/AppContext'
+import FormControl from '@mui/material/FormControl'
+import MenuItem from '@mui/material/MenuItem'
+import QuickMenuEventSettings from '../components/QuickMenuEventSettings'
+import QuickMenuBulkAdd from '../components/QuickMenuBulkAdd'
+import QuickMenuBulkEdit from '../components/QuickMenuBulkEdit'
+import Box from '@mui/material/Box'
 
 /* TODO fetch rosterPlan from BE (cached request) with swr
     Then fetch time slots and infos... then everything
@@ -34,11 +44,12 @@ const MeetingEdit = () => {
 }
 
 const Inner = ({ meetingId }) => {
-  const { context } = useContext(AppContext)
-
-  if (_.isEqual(context.user, defaultAppContext.user)) {
-    throw new Error(ERROR_MESSAGES.PLEASE_SIGN_IN)
-  }
+  const menuPages = [
+    ['configurations', 'Meeting Configurations'],
+    ['add', 'Bulk Add Events'],
+    ['edit', 'Bulk Edit Events']
+  ]
+  const [currMenu, setCurrMenu] = useState(null)
 
   if (meetingId === undefined) {
     throw new Error(ERROR_MESSAGES.INVALID_URL)
@@ -48,21 +59,47 @@ const Inner = ({ meetingId }) => {
   if (error) throw new Error(error)
   if (!data) return <CircularProgress />
 
+  const handleMenuChange = e => {
+    setCurrMenu(e.target.value)
+  }
+
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback} resetKeys={[data]}>
       <PageTitle pageTitle={data?.title} />
       <Grid container spacing={5}>
         <Grid item xs={12} lg={8}>
-          <Typography variant='h5' sx={{ my: 2 }}>
-            Schedule
-          </Typography>
-          <CustomScheduleComponent rosterPlan={data} />
+          <Box>
+            <Box sx={{ flexDirection: 'row' }}>
+              <Typography variant='h5' sx={{ my: 2 }}>
+                Quick Menu
+              </Typography>
+              <FormControl sx={{ mx: 1 }}>
+                <Select
+                  value={currMenu}
+                  onChange={handleMenuChange}
+                >
+                  {menuPages.map(([value, description]) => <MenuItem key={value} value={value}>{description}</MenuItem>)}
+                </Select>
+                <FormHelperText>Without label</FormHelperText>
+              </FormControl>
+            </Box>
+            {currMenu === 'configurations' && <QuickMenuEventSettings />}
+            {currMenu === 'add' && <QuickMenuBulkAdd />}
+            {currMenu === 'edit' && <QuickMenuBulkEdit />}
+          </Box>
+          <Divider variant='middle' />
+          <Box>
+            <Typography variant='h5' sx={{ my: 2 }}>
+              Schedule
+            </Typography>
+            <CustomScheduleComponent rosterPlan={data} />
+          </Box>
         </Grid>
         <Grid item xs={12} lg={4}>
           <Typography variant='h5' sx={{ my: 2 }}>
             Invitations
           </Typography>
-          <InvitationGrid rosterPlan={data} invitationGrid={{ readonly: false }} />
+          <InvitationGrid rosterPlan={data} invitationGrid={{ readonly: false }}/>
         </Grid>
       </Grid>
     </ErrorBoundary>
--- src/main/webapp/app/src/pages/MeetingRank.jsx
+++ src/main/webapp/app/src/pages/MeetingRank.jsx
@@ -1,36 +1,19 @@
 import * as React from 'react'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import PageTitle from '../components/PageTitle'
 import { useParams } from 'react-router-dom'
 import ErrorFallback from '../components/ErrorFallback'
 import { ErrorBoundary } from 'react-error-boundary'
 import CustomScheduleComponent from '../components/CustomScheduleComponent'
 import useSWR from 'swr'
-import {
-  CircularProgress,
-  Divider,
-  FormHelperText,
-  Select,
-} from '@mui/material'
+import { CircularProgress } from '@mui/material'
 import ajax from '../core/ajax'
 import InvitationGrid from '../components/InvitationGrid'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { API, ERROR_MESSAGES } from '../core/const'
-import AppContext from '../core/AppContext'
-import FormControl from '@mui/material/FormControl'
-import MenuItem from '@mui/material/MenuItem'
-import QuickMenuEventSettings from '../components/QuickMenuEventSettings'
-import QuickMenuBulkAdd from '../components/QuickMenuBulkAdd'
-import QuickMenuBulkEdit from '../components/QuickMenuBulkEdit'
-import Box from '@mui/material/Box'
-
-/* TODO fetch rosterPlan from BE (cached request) with swr
-    Then fetch time slots and infos... then everything
-    Use DM to connect component to CRUD API
-    Include Grid of users invited
-    [Optimization] can use projection if too many requests
-*/
+import AppContext, { defaultAppContext } from '../core/AppContext'
+import _ from 'lodash'
 
 const MeetingRank = () => {
   const { context } = useContext(AppContext)
@@ -44,6 +27,12 @@ const MeetingRank = () => {
 }
 
 const Inner = ({ meetingId }) => {
+  const { context } = useContext(AppContext)
+
+  if (_.isEqual(context.user, defaultAppContext.user)) {
+    throw new Error(ERROR_MESSAGES.PLEASE_SIGN_IN)
+  }
+
   if (meetingId === undefined) {
     throw new Error(ERROR_MESSAGES.INVALID_URL)
   }
@@ -52,56 +41,21 @@ const Inner = ({ meetingId }) => {
   if (error) throw new Error(error)
   if (!data) return <CircularProgress />
 
-  const [currMenu, setCurrMenu] = useState('EventSettings')
-  const menuPages = {
-    configurations: 'Meeting Configurations',
-    add: 'Bulk Add Events',
-    edit: 'Bulk Edit Events'
-  }
-
-  const handleMenuChange = e => {
-    setCurrMenu(e.target.value)
-  }
-
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback} resetKeys={[data]}>
       <PageTitle pageTitle={data?.title} />
       <Grid container spacing={5}>
         <Grid item xs={12} lg={8}>
-          <Box>
-            <Box sx={{ flexDirection: 'row' }}>
-              <Typography variant='h5' sx={{ my: 2 }}>
-                Quick Menu
-              </Typography>
-              <FormControl sx={{ mx: 1 }}>
-                <Select
-                  value={currMenu}
-                  onChange={handleMenuChange}
-                >
-                  <MenuItem value='configurations'>{menuPages.configurations}</MenuItem>
-                  <MenuItem value='add'>{menuPages.add}</MenuItem>
-                  <MenuItem value='edit'>{menuPages.edit}</MenuItem>
-                </Select>
-                <FormHelperText>Without label</FormHelperText>
-              </FormControl>
-            </Box>
-            {(currMenu === 'configurations' && QuickMenuEventSettings) ||
-              (currMenu === 'add' && QuickMenuBulkAdd) ||
-              (currMenu === 'edit' && QuickMenuBulkEdit)}
-          </Box>
-          <Divider variant='middle' />
-          <Box>
-            <Typography variant='h5' sx={{ my: 2 }}>
-              Schedule
-            </Typography>
-            <CustomScheduleComponent rosterPlan={data} />
-          </Box>
+          <Typography variant='h5' sx={{ my: 2 }}>
+            Schedule
+          </Typography>
+          <CustomScheduleComponent rosterPlan={data} />
         </Grid>
         <Grid item xs={12} lg={4}>
           <Typography variant='h5' sx={{ my: 2 }}>
             Invitations
           </Typography>
-          <InvitationGrid rosterPlan={data} />
+          <InvitationGrid rosterPlan={data} invitationGrid={{ readonly: true }} />
         </Grid>
       </Grid>
     </ErrorBoundary>
