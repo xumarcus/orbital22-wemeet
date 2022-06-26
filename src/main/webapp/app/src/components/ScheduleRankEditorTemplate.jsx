0d100f725325e64141ff04a7e49f0637ec1ef96b
marcus <marcus@workforceoptimizer.com>
2022-06-26 19:23:23 +0800

    Fix template

--- src/main/webapp/app/src/components/ScheduleRankEditorTemplate.jsx
+++ src/main/webapp/app/src/components/ScheduleRankEditorTemplate.jsx
@@ -8,19 +8,33 @@ const ScheduleRankEditorTemplate = (props) => {
   const { context } = useContext(AppContext)
 
   if (_.isEmpty(props)) {
-    return <div></div>
+    return <div />
   }
 
   const RankEditorRow = ({ label, name, ...props }) => (
     <tr>
-      <td className="e-textlabel">{label}</td>
+      <td className='e-textlabel'>{label}</td>
       <td colSpan={4}>
         <input
-          className="e-field e-input"
+          className='e-field e-input'
           id={name}
           name={name}
           style={{ width: '100%' }}
           {...props}
+        />
+      </td>
+    </tr>
+  )
+
+  const DateTimePickerEditorRow = ({ label, name, ...props }) => (
+    <tr>
+      <td className='e-textlabel'>{label}</td>
+      <td colSpan={4}>
+        <DateTimePickerComponent
+          className='e-field'
+          format='dd/MM/yy hh:mm a'
+          id={name}
+          data-name={name}
         />
       </td>
     </tr>
@@ -32,7 +46,7 @@ const ScheduleRankEditorTemplate = (props) => {
     startDateTime,
     endDateTime,
     capacity,
-    timeSlotUserInfos,
+    timeSlotUserInfos
   } = props
 
   // TODO refactor into BE
@@ -40,44 +54,31 @@ const ScheduleRankEditorTemplate = (props) => {
     info => info.user.id === context.user.id)?.rank
 
   return (
-    <table className="custom-event-editor"
-           style={{ width: '100%', cellpadding: '5' }}>
-
-
+    <table
+      className='custom-event-editor'
+      style={{ width: '100%', cellpadding: '5' }}
+    >
       <tbody>
-      <RankEditorRow label="TimeSlotId" name="timeSlotId" type="number"
-                     value={timeSlotId} disabled hidden/>
-      <RankEditorRow label="Capacity" name="capacity" value={capacity}
-                     disabled/>
-      <RankEditorRow label="Rank" name="rank" defaultValue={rank}
-                     type="number"/>
-
-      <tr>
-        <td className="e-textlabel">From</td>
-        <td colSpan={4}>
-          <DateTimePickerComponent
-            className="e-field"
-            format="dd/MM/yy hh:mm a"
-            id="startDateTime"
-            data-name="startDateTime"
-            value={startDateTime}
-            disabled
-          />
-        </td>
-      </tr>
-      <tr>
-        <td className="e-textlabel">To</td>
-        <td colSpan={4}>
-          <DateTimePickerComponent
-            className="e-field"
-            format="dd/MM/yy hh:mm a"
-            id="endDateTime"
-            data-name="endDateTime"
-            value={endDateTime}
-            disabled
-          />
-        </td>
-      </tr>
+        <RankEditorRow
+          label='TimeSlotId' name='timeSlotId' type='number'
+          value={timeSlotId} disabled hidden
+        />
+        <RankEditorRow
+          label='Capacity' name='capacity' value={capacity}
+          disabled
+        />
+        <RankEditorRow
+          label='Rank' name='rank' defaultValue={rank}
+          type='number'
+        />
+        <DateTimePickerEditorRow
+          label='From' name='startDateTime' value={startDateTime}
+          disabled
+        />
+        <DateTimePickerEditorRow
+          label='To' name='endDateTime' value={endDateTime}
+          disabled
+        />
       </tbody>
     </table>
   )
