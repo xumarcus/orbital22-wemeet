dc356bb177cecf537f762a5020a40cdaf489c54e
marcus <marcus@workforceoptimizer.com>
2022-07-08 01:53:25 +0800

    Simplify /api/users/me

--- src/main/java/com/orbital22/wemeet/security/CustomUserDetailsService.java
+++ src/main/java/com/orbital22/wemeet/security/CustomUserDetailsService.java
@@ -8,22 +8,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
- * @author xumarcus
- * <p>
- * This DAO furnishes UserDetails to Spring Security.
- * There is no need to inject this elsewhere.
+ * @author xumarcus This DAO furnishes UserDetails to Spring Security. There is no need to inject
+ *     this elsewhere.
  */
-
 @Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private UserRepository userRepository;
+  private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String email) {
+  @Override
+  public UserDetails loadUserByUsername(String email) {
     return userRepository
         .findByEmail(email)
         .map(CustomUser::new)
         .orElseThrow(() -> new UsernameNotFoundException(email));
-    }
+  }
 }
