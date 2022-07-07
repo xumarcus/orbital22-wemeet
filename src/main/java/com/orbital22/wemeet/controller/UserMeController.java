8d26c8d67a54e8f3cfaf8e95cf544223a9dea633
marcus <marcus@workforceoptimizer.com>
2022-07-08 01:52:48 +0800

    Split controllers and remove workaround

---
+++ src/main/java/com/orbital22/wemeet/controller/UserMeController.java
@@ -0,0 +1,19 @@
+package com.orbital22.wemeet.controller;
+
+import com.orbital22.wemeet.security.CustomUser;
+import lombok.AllArgsConstructor;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@BasePathAwareController
+@AllArgsConstructor
+public class UserMeController {
+  @GetMapping("/users/me")
+  @PreAuthorize("#authentication != null and #authentication.isAuthenticated()")
+  String me(Authentication authentication) {
+    CustomUser customUser = (CustomUser) authentication.getPrincipal();
+    return "/api/users/" + customUser.getUser().getId();
+  }
+}
