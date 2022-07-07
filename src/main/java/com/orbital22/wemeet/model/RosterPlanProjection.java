d48bf9dc554110e9a0fe7ba70e18ba7ea939ac56
marcus <marcus@workforceoptimizer.com>
2022-07-08 01:52:19 +0800

    Implements Go Back

---
+++ src/main/java/com/orbital22/wemeet/model/RosterPlanProjection.java
@@ -0,0 +1,24 @@
+package com.orbital22.wemeet.model;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Set;
+
+@Projection(
+    name = "rosterPlanProjection",
+    types = {RosterPlan.class})
+public interface RosterPlanProjection {
+  int getId();
+
+  String getTitle();
+
+  RosterPlan getParent();
+
+  User getOwner();
+
+  Set<TimeSlotProjection> getTimeSlots();
+
+  Set<RosterPlanUserInfoProjection> getRosterPlanUserInfos();
+
+  Boolean getSolved();
+}
