c3d2fb747bd58742409686a2512393a6d5368eb2
marcus <marcus@workforceoptimizer.com>
2022-07-11 01:35:36 +0800

    Fix react app controller /

--- src/main/java/com/orbital22/wemeet/controller/ReactAppController.java
+++ src/main/java/com/orbital22/wemeet/controller/ReactAppController.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @Controller
 public class ReactAppController {
-  @RequestMapping(value = { "/", "/{head:[\\w\\-]+}", "/{head:^(?!api$).*$}/*/{tail:[\\w\\-]+}","/error"  })
-  String index(@PathVariable String head, @PathVariable String tail) {
+  @RequestMapping(
+      value = {"/{head:[\\w\\-]+}", "/{head:^(?!api$).*$}/*/{tail:[\\w\\-]+}", "/error"})
+  String index(
+      @PathVariable Optional<String> head,
+      @PathVariable Optional<String> tail,
+      HttpServletRequest request) {
     return "/index.html";
   }
 }
