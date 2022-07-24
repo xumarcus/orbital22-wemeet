ea421cf5c42513555f4589b6226c549784ff0626
marcus <marcus@workforceoptimizer.com>
2022-07-24 17:32:28 +0800

    Update link text

--- src/main/webapp/app/src/pages/Guide.jsx
+++ src/main/webapp/app/src/pages/Guide.jsx
@@ -3,15 +3,18 @@ import * as React from 'react'
 import CenterWrapper from '../components/core/CenterWrapper'
 import Link from '@mui/material/Link'
 
+// TODO make guide just a link to documentation index? Instead of a page?
 const Guide = () => (
   <CenterWrapper>
     {/* button group */}
-    <Typography variant='h3' align='center'>
-      User Guide Coming Soon
-    </Typography>
+    {
+      <Typography variant='h3' align='center'>
+        User Guide Coming Soon
+      </Typography>
+    }
 
     <Link variant='h3' align='center' href='/static/docs/index.html'>
-      Developer Guide
+      Documentation
     </Link>
   </CenterWrapper>
 )
